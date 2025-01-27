6c4c59411ff6e49b39c5adad28261f310307b89d
Ehsan Hosseiny Nezhad <ehsan.hosinynezhad@yahoo.com>
2025-01-27 20:18:25 +0000

    Add files via upload

--- app/components/page1/Comp_1_5.tsx
+++ app/components/page1/Comp_1_5.tsx
@@ -1,5 +1,18 @@
+
+import Comp_1_4 from '../../components/page1/Comp_1_4';
+
 // components/page1/Comp_1_5.js
 export default function Comp_1_5() {
-    return <div>Component 1.5</div>;
-  }
+  const images = ['/images/app_law.png', '/images/app_law2.png'];
+
+  return  <div >
+    <div className='flex' > 
+      <Comp_1_4 imageSrc1="/images/app_law.png" imageSrc2="/images/LSTM1.png"/>
+      <Comp_1_4 imageSrc1="/images/LSTM1.png" imageSrc2="/images/app_law.png"/>
+    </div>  
+
+
+  </div>
+
   
+};
